343a0eff39f5792d5e5c29555c0d31d800c01418
Duma Alex <alex.duma123@gmail.com>
2019-10-24 19:29:06 +0300

    Merge dev into master.

--- Server/sensor/views.py
+++ Server/sensor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
 
 
 # Create your views here.
 def index(request):
     return render(request, 'sensor/index.html', {})
+
+
+def base(request, base_name):
+    return render(request, 'sensor/base.html', {
+        'base_name_json': mark_safe(json.dumps(base_name))
+    })
